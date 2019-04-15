dd0548ffb81abb4264ce18d6d8a7a9b284f408c6
anol <anol@users.noreply.github.com>
2019-04-16 01:19:52 +0200

    Fixed pwm puls width

--- TM4C-SteinsMotor-2/PwmGenerator.cpp
+++ TM4C-SteinsMotor-2/PwmGenerator.cpp
@@ -18,9 +18,9 @@ class PwmConfiguration
 {
 public:
     PwmConfiguration(std::string tag, uint32_t periph_pwm, uint32_t periph_gpio, uint32_t gpio_pwm_pin, uint32_t gpio_base, uint32_t gpio_pin,
-                     uint32_t pwm_base, uint32_t pwm_gen, uint32_t pwm_out_bit) :
+                     uint32_t pwm_base, uint32_t pwm_gen, uint32_t pwm_out, uint32_t pwm_out_bit) :
             m_tag(tag), m_periph_pwm(periph_pwm), m_periph_gpio(periph_gpio), m_gpio_pwm_pin(gpio_pwm_pin), m_gpio_base(gpio_base), m_gpio_pin(
-                    gpio_pin), m_pwm_base(pwm_base), m_pwm_gen(pwm_gen), m_pwm_out_bit(pwm_out_bit)
+                    gpio_pin), m_pwm_base(pwm_base), m_pwm_gen(pwm_gen), m_pwm_out(pwm_out), m_pwm_out_bit(pwm_out_bit)
     {
 
     }
@@ -32,18 +32,20 @@ public:
     uint32_t m_gpio_pin;
     uint32_t m_pwm_base;
     uint32_t m_pwm_gen;
+    uint32_t m_pwm_out;
     uint32_t m_pwm_out_bit;
 };
 
 const PwmConfiguration Configurations[] =
-{
-    {   "Red", SYSCTL_PERIPH_PWM1, SYSCTL_PERIPH_GPIOF, GPIO_PF1_M1PWM5, GPIO_PORTF_BASE, GPIO_PIN_1, PWM1_BASE, PWM_GEN_2, PWM_OUT_5_BIT},
-    {   "Blue", SYSCTL_PERIPH_PWM1, SYSCTL_PERIPH_GPIOF, GPIO_PF2_M1PWM6, GPIO_PORTF_BASE, GPIO_PIN_2, PWM1_BASE, PWM_GEN_3, PWM_OUT_6_BIT},
-    {   "Green", SYSCTL_PERIPH_PWM1, SYSCTL_PERIPH_GPIOF, GPIO_PF3_M1PWM7, GPIO_PORTF_BASE, GPIO_PIN_3, PWM1_BASE, PWM_GEN_3, PWM_OUT_7_BIT},
-    {   "PD0", SYSCTL_PERIPH_PWM1, SYSCTL_PERIPH_GPIOD, GPIO_PD0_M1PWM0, GPIO_PORTD_BASE, GPIO_PIN_0, PWM1_BASE, PWM_GEN_0, PWM_OUT_0_BIT},
-    {   "PD1", SYSCTL_PERIPH_PWM1, SYSCTL_PERIPH_GPIOD, GPIO_PD1_M1PWM1, GPIO_PORTD_BASE, GPIO_PIN_1, PWM1_BASE, PWM_GEN_0, PWM_OUT_1_BIT},
-    {   "PB6", SYSCTL_PERIPH_PWM0, SYSCTL_PERIPH_GPIOB, GPIO_PB6_M0PWM0, GPIO_PORTB_BASE, GPIO_PIN_6, PWM0_BASE, PWM_GEN_3, PWM_OUT_6_BIT},
-    {   "PB7", SYSCTL_PERIPH_PWM0, SYSCTL_PERIPH_GPIOB, GPIO_PB7_M0PWM1, GPIO_PORTB_BASE, GPIO_PIN_7, PWM0_BASE, PWM_GEN_3, PWM_OUT_7_BIT}};
+
+{ { "Red", SYSCTL_PERIPH_PWM1, SYSCTL_PERIPH_GPIOF, GPIO_PF1_M1PWM5, GPIO_PORTF_BASE, GPIO_PIN_1, PWM1_BASE, PWM_GEN_2, PWM_OUT_5, PWM_OUT_5_BIT },
+  { "Blue", SYSCTL_PERIPH_PWM1, SYSCTL_PERIPH_GPIOF, GPIO_PF2_M1PWM6, GPIO_PORTF_BASE, GPIO_PIN_2, PWM1_BASE, PWM_GEN_3, PWM_OUT_6, PWM_OUT_6_BIT },
+
+  { "Green", SYSCTL_PERIPH_PWM1, SYSCTL_PERIPH_GPIOF, GPIO_PF3_M1PWM7, GPIO_PORTF_BASE, GPIO_PIN_3, PWM1_BASE, PWM_GEN_3, PWM_OUT_7, PWM_OUT_7_BIT },
+  { "PD0", SYSCTL_PERIPH_PWM1, SYSCTL_PERIPH_GPIOD, GPIO_PD0_M1PWM0, GPIO_PORTD_BASE, GPIO_PIN_0, PWM1_BASE, PWM_GEN_0, PWM_OUT_0, PWM_OUT_0_BIT },
+  { "PD1", SYSCTL_PERIPH_PWM1, SYSCTL_PERIPH_GPIOD, GPIO_PD1_M1PWM1, GPIO_PORTD_BASE, GPIO_PIN_1, PWM1_BASE, PWM_GEN_0, PWM_OUT_1, PWM_OUT_1_BIT },
+  { "PB6", SYSCTL_PERIPH_PWM0, SYSCTL_PERIPH_GPIOB, GPIO_PB6_M0PWM0, GPIO_PORTB_BASE, GPIO_PIN_6, PWM0_BASE, PWM_GEN_3, PWM_OUT_6, PWM_OUT_6_BIT },
+  { "PB7", SYSCTL_PERIPH_PWM0, SYSCTL_PERIPH_GPIOB, GPIO_PB7_M0PWM1, GPIO_PORTB_BASE, GPIO_PIN_7, PWM0_BASE, PWM_GEN_3, PWM_OUT_7, PWM_OUT_7_BIT } };
 
 const PwmConfiguration* p_conf = 0;
 
@@ -69,7 +71,7 @@ void PwmGenerator::Initialize()
     GPIOPinTypePWM(p_conf->m_gpio_base, p_conf->m_gpio_pin);
     PWMGenConfigure(p_conf->m_pwm_base, p_conf->m_pwm_gen, PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
     PWMGenPeriodSet(p_conf->m_pwm_base, p_conf->m_pwm_gen, 64000);
-    PWMPulseWidthSet(p_conf->m_pwm_base, p_conf->m_pwm_out_bit, 0);
+    PWMPulseWidthSet(p_conf->m_pwm_base, p_conf->m_pwm_out, 0);
     PWMOutputState(p_conf->m_pwm_base, p_conf->m_pwm_out_bit, true);
     PWMGenEnable(p_conf->m_pwm_base, p_conf->m_pwm_gen);
 }
@@ -80,7 +82,7 @@ void PwmGenerator::Invert(bool invert)
 void PwmGenerator::SetPulseWidth(int percent)
 {
     uint32_t ui32Width = PWMGenPeriodGet(p_conf->m_pwm_base, p_conf->m_pwm_gen) * percent / 100;
-    PWMPulseWidthSet(p_conf->m_pwm_base, p_conf->m_pwm_out_bit, ui32Width);
+    PWMPulseWidthSet(p_conf->m_pwm_base, p_conf->m_pwm_out, ui32Width);
 }
 void PwmGenerator::Enable()
 {
